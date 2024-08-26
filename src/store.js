cc56ea0f97fb8640dee92e36ab5ac1ddaf695e8f
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-08-26 09:38:38 -0600

    Added new initialStateCustomer object and updated reducer function to use initialStateAccount"

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
 import { createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function reducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
